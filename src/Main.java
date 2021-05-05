23719b0fc2cf992b7d48a1596427af4b6934c6ea
Armani Catlin <k1ngsoap23@gmail.com>
2021-05-05 13:13:28 -0500

    Third Day Changes

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,59 @@
 class Hospital {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor("Fumi", 33, 150000, 13, 1, "Senior Consultant");
-        Nurse nurse = new Nurse("Armani", 34, 60000, 8, 1, "Technical Practitioner");
-        Administrator administrator = new Administrator("Mehak", 30, 100000, 10, 1, "Financial Administrator");
-        Receptionist receptionist = new Receptionist("Isaiah", 32, 40000, 13, 1, "Schedule Manager");
-        Janitor janitor = new Janitor("Joseph", 28, 40000000, 10, 1, "Sanitation Manager");
+        Doctor doctor = new Doctor("Fumi", 33, 150000, 13, 2, "Senior Consultant");
+        Surgeon surgeon = new Surgeon("Jeremy", 30, 125000, 11, 2, "Dissective Specialist");
+        Nurse nurse = new Nurse("Armani", 34, 40000, 8, 2, "Technical Practitioner");
+        Administrator administrator = new Administrator("Mehak", 30, 100000, 10, 3, "Financial Administrator");
+        Receptionist receptionist = new Receptionist("Isaiah", 32, 50000, 13, 1, "Schedule Manager");
+        Janitor janitor = new Janitor("Joseph", 28, 40000, 10, 1, "Sanitation Manager");
 
         //Doctor
-        System.out.println(doctor.getAge());
         System.out.println(doctor.getName());
-        System.out.println(doctor.getSalary());
         System.out.println(doctor.getTitle());
+        System.out.println(doctor.getAge());
+        System.out.println(doctor.getSalary());
         System.out.println(doctor.getWorkFloor());
         System.out.println(doctor.getYearsOfExperience());
         doctor.diagnose();
+        //Surgeon
+        System.out.println(surgeon.getName());
+        System.out.println(surgeon.getTitle());
+        System.out.println(surgeon.getAge());
+        System.out.println(surgeon.getSalary());
+        System.out.println(surgeon.getWorkFloor());
+        System.out.println(surgeon.getYearsOfExperience());
+        surgeon.diagnose();
+        surgeon.operate();
         //Nurse
-        System.out.println(nurse.getAge());
         System.out.println(nurse.getName());
-        System.out.println(nurse.getSalary());
         System.out.println(nurse.getTitle());
+        System.out.println(nurse.getAge());
+        System.out.println(nurse.getSalary());
         System.out.println(nurse.getWorkFloor());
         System.out.println(nurse.getYearsOfExperience());
         nurse.examine();
         //Administrator
-        System.out.println(administrator.getAge());
         System.out.println(administrator.getName());
-        System.out.println(administrator.getSalary());
         System.out.println(administrator.getTitle());
+        System.out.println(administrator.getAge());
+        System.out.println(administrator.getSalary());
         System.out.println(administrator.getWorkFloor());
         System.out.println(administrator.getYearsOfExperience());
         administrator.budget();
         //Receptionist
-        System.out.println(receptionist.getAge());
         System.out.println(receptionist.getName());
-        System.out.println(receptionist.getSalary());
         System.out.println(receptionist.getTitle());
+        System.out.println(receptionist.getAge());
+        System.out.println(receptionist.getSalary());
         System.out.println(receptionist.getWorkFloor());
         System.out.println(receptionist.getYearsOfExperience());
         receptionist.type();
         receptionist.call();
         //Janitor
-        System.out.println(janitor.getAge());
         System.out.println(janitor.getName());
-        System.out.println(janitor.getSalary());
         System.out.println(janitor.getTitle());
+        System.out.println(janitor.getAge());
+        System.out.println(janitor.getSalary());
         System.out.println(janitor.getWorkFloor());
         System.out.println(janitor.getYearsOfExperience());
         janitor.clean();
@@ -103,7 +113,6 @@ class Employee {
     public void setTitle(String title) {
         this.title = title;
     }
-
     public void set_salary(int salary) {
         this.salary = salary;
     }
@@ -114,7 +123,19 @@ class Doctor extends Employee {
         super(name, age, salary, yearsOfExperience, workFloor, title);
     }
     public void diagnose() {
-        System.out.println("Runs the diagnosis to analyse the ailment.");
+        System.out.println("She runs the symptomatic diagnosis to analyse the ailment.");
+    }
+}
+
+class Surgeon extends Employee {
+    Surgeon(String name, int age, int salary, int yearsOfExperience, int workFloor, String title) {
+        super(name, age, salary, yearsOfExperience, workFloor, title);
+    }
+    public void diagnose() {
+        System.out.println("He runs systematic diagnosis to analyse the ailment.");
+    }
+    public void operate() {
+        System.out.println("Then based on the diagnosis, performs the procedure needed to improve the patient's condition.");
     }
 }
 
@@ -123,7 +144,7 @@ class Nurse extends Employee {
         super(name, age, salary, yearsOfExperience, workFloor, title);
     }
     public void examine() {
-        System.out.println("Examines the patient for issues.");
+        System.out.println("He examines the patient for issues.");
     }
 }
 
@@ -132,7 +153,7 @@ class Administrator extends Employee {
         super(name, age, salary, yearsOfExperience, workFloor, title);
     }
     public void budget() {
-        System.out.println("Handles the budgeting for the hospital.");
+        System.out.println("She handles the budgeting for the hospital and manages expenses,");
     }
 }
 
@@ -141,10 +162,10 @@ class Receptionist extends Employee {
         super(name, age, salary, yearsOfExperience, workFloor, title);
     }
     public void type() {
-        System.out.println("Files and documents the issue to establish a record.");
+        System.out.println("He files and documents the issue to establish a record.");
     }
     public void call() {
-        System.out.println("Manages the calls of patients to schedule appointments.");
+        System.out.println("He also manages the calls of patients to schedule appointments and maintain a stable time frame.");
     }
 }
 
@@ -153,6 +174,6 @@ class Janitor extends Employee {
         super(name, age, salary, yearsOfExperience, workFloor, title);
     }
     public void clean() {
-        System.out.println("Cleans the grounds and ensures sanitation.");
+        System.out.println("He cleans the grounds and ensures sanitation to avoid spread of disease through the hospital.");
     }
 }
